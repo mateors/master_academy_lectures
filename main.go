1dc0ce72af10acfbd56bd045311ce8ba85013e9b
mateors <bill.rassel@gmail.com>
2021-02-05 21:19:44 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+//fmt.Println("Hello bangladesh")
+
+	//primitive data types == আদিম == স্বরবর্ণ
+	//Built-in Types
+	//Basic types
+	//স্বরবর্ণ
+	//https://bn.wikipedia.org/wiki/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE_%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%B0%E0%A6%AC%E0%A6%B0%E0%A7%8D%E0%A6%A3
+
+type TOKEN = string
+
+var tag TOKEN
+tag="popop"
+fmt.Println(tag)
+
+}
